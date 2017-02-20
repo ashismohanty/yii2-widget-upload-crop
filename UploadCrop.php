1fd8c405d4e663ed91f64a3da12e8d5ab6ccd07d
Ashis <ashis@printgreener.com>
2017-02-20 12:55:54 +0530

    First upload with Bootstrap v3.3.7 support.

---
+++ UploadCrop.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * UploadCrop Widget
+ *
+ * @author Joseba Juaniz <joseba.juaniz@gmail.com>
+ * @since 1.0
+ *
+ * @author Ashis Mohanty <ashismohanty82@gmail.com>
+ * @since 2.0
+ */
+
+namespace cyneek\yii2\widget\upload\crop;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\base\Widget;
+use yii\bootstrap\Modal;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\widgets\ActiveForm;
+
+class UploadCrop extends Widget
+{
+	/** @var \yii\db\ActiveRecord */
+	var $model;
+
+	/** @var  string */
+	var $attribute;
+
+	/* @var ActiveForm */
+	var $form = NULL;
+
+	/** @var boolean */
+	var $enableClientValidation;
+
+	/** @var array */
+	var $imageOptions;
+
+	/** @var array */
+	var $jcropOptions = array();
+
+	/** @var integer */
+	var $maxSize = 300;
+
+	/** @var string */
+	var $defaultPreviewImg = NULL;
+
+	/**
+	 * only call this method after a form closing and
+	 *    when user hasn't used in the widget call the parameter $form
+	 *    this adds to every form in the view the field validation.
+	 *
+	 * @param array $config
+	 * @return string
+	 * @throws \yii\base\InvalidConfigException
+	 */
+	static function manualValidation($config = [])
+	{
+
+		if (!array_key_exists('model', $config) || !array_key_exists('attribute', $config)) {
+			throw new InvalidParamException('Config array must have a model and attribute.');
+		}
+
+		$view = Yii::$app->getView();
+		$field_id = Html::getInputId($config['model'], $config['attribute']);
+		$view->registerJs('$("#' . $field_id . '").urlParser("launchValidation");');
+	}
+
+
+	/**
+	 * Renders the field.
+	 */
+	public function run()
+	{
+		if (is_null($this->imageOptions))
+		{
+			$this->imageOptions = [
+				'alt' => 'Crop this image'
+			];
+		}
+
+		$this->imageOptions['id'] = Yii::$app->getSecurity()->generateRandomString(10);
+
+		$inputField = Html::getInputId($this->model, $this->attribute, ['data-image_id' => $this->imageOptions['id']]);
+
+		$default_jcropOptions = [
+								'dashed' => FALSE,
+								'zoomable' => FALSE,
+								'rotatable' => FALSE];
+
+
+		$this->jcropOptions = array_merge($default_jcropOptions, $this->jcropOptions);
+
+
+		if (is_null($this->form))
+		{
+			$this->form = new ActiveForm();
+
+			if (!is_null($this->enableClientValidation))
+			{
+				$this->form->enableClientValidation = $this->enableClientValidation;
+			}
+		}
+
+		echo Html::beginTag('div', ['class' => 'uploadcrop']);
+		echo $this->form->field($this->model, $this->attribute)->fileInput();	
+		echo html::hiddenInput($this->attribute.'_data', '', ['class' => 'cropper-data']);
+
+
+			echo Html::beginTag('div', ['class' => 'preview-pane']);
+				echo Html::beginTag('div', ['class' => 'preview-container']);
+					//echo Html::img('', ['class' => 'preview_image']);
+					if (!is_null($this->defaultPreviewImg))
+					{
+						$defaultPreviewImgOptions = [							
+							'id' => Yii::$app->getSecurity()->generateRandomString(10),
+							'class' => 'preview_image'
+						];
+						echo Html::img($this->defaultPreviewImg, $defaultPreviewImgOptions);
+					}
+				echo Html::endTag('div');
+			echo Html::endTag('div');
+
+		Modal::begin([
+			'id' => 'cropper-modal-'. $this->imageOptions['id'],
+			'header' => '<h2>Crop image</h2>',
+			'closeButton' => [],
+			'footer' => '<div class="row cropper-btns"><div class="col-md-6">' . Html::button('Cancel', ['id' => $this->imageOptions['id'].'_button_cancel', 'class' => 'btn btn-default btn-block', 'data-dismiss' => 'modal']) . '</div><div class="col-md-6">' . Html::button('Accept', ['id' => $this->imageOptions['id'].'_button_accept', 'class' => 'btn btn-primary btn-block cropper-done']) . '</div></div>',
+			'size' => Modal::SIZE_LARGE,
+		]);
+
+			echo Html::beginTag('div', ['id' => 'image-source'.$this->imageOptions['id'], 'class' => 'row cropper-body']);
+				
+				//Image crop area
+				echo Html::beginTag('div', ['class' => 'col-md-9']);
+					echo Html::beginTag('div', ['class' => 'cropper-wrapper']);
+						//echo Html::img('', $this->imageOptions);
+					echo Html::endTag('div');
+				echo Html::endTag('div');
+
+				//preview column
+				echo Html::beginTag('div', ['class' => 'col-md-3']);
+					echo Html::beginTag('div', ['class' => 'cropper-preview preview-lg']);
+					echo Html::endTag('div');
+				echo Html::endTag('div');
+				
+			echo Html::endTag('div');
+
+			// echo html::hiddenInput($this->attribute.'-cropping[x]', '', ['id' => $inputField.'-x']);
+			// echo html::hiddenInput($this->attribute.'-cropping[width]','', ['id' => $inputField.'-width']);
+			// echo html::hiddenInput($this->attribute.'-cropping[y]', '', ['id' => $inputField.'-y']);
+			// echo html::hiddenInput($this->attribute.'-cropping[height]','', ['id' => $inputField.'-height']);
+
+		Modal::end();
+
+		echo Html::endTag('div');
+
+		$view = $this->getView();
+
+		UploadCropAsset::register($view);
+
+		$jcropOptions = ['inputField' => $inputField, 'jcropOptions' => $this->jcropOptions];
+
+		$jcropOptions['maxSize'] = $this->maxSize;
+
+		$jcropOptions = Json::encode($jcropOptions);
+
+		$view->registerJs('jQuery("#'.$inputField.'").uploadCrop('.$jcropOptions.');');
+	}
+}
---
+++ assets/js/uploadcrop.js
@@ -0,0 +1,290 @@
+/**
+ * Uses cropper javascript widget from https://github.com/fengyuanchen/cropper
+ *
+ * Author: Joseba Juaniz
+ * Year: 2015
+ * 
+ * Modified by: Ashis Kumar Mohanty
+ * Year: 2017
+ */
+
+(function(factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node / CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals.
+        factory(jQuery);
+    }
+})(function($) {
+
+    'use strict';
+
+    var console = window.console || { log: function() {} };
+
+    function CropAvatar($element, options) {
+
+        //cropper options
+        this._jcropOptions = options['jcropOptions'];
+
+        //main upload container
+        this.$container = $element.parents('.uploadcrop');
+
+        //image preview section
+        this.$avatarView = this.$container.find('.preview-pane');
+        this.$avatarViewContainer = this.$avatarView.find('.preview-container');
+
+        //modal dialog
+        this.$avatarModal = this.$container.find('div.modal');
+
+        //modal dialog loading
+        this.$loading = this.$container.find('.loading');
+
+        //modal dialog body
+        this.$avatarModalBody = this.$avatarModal.find('.modal-body');
+
+        // this.$avatarSrc = this.$container.find('.cropper-src');
+        this.$avatarData = this.$container.find('.cropper-data');
+        // this.$avatarInput = this.$container.find('.cropper-input');
+        this.$avatarInput = $('#' + options['inputField']);
+        this.$avatarDone = this.$container.find('.cropper-done');
+        this.$avatarBtns = this.$container.find('.cropper-btns');
+
+        this.$avatarWrapper = this.$avatarModal.find('.cropper-wrapper');
+        this.$avatarPreview = this.$avatarModal.find('.cropper-preview');
+
+        this.init();
+    }
+
+    CropAvatar.prototype = {
+        constructor: CropAvatar,
+
+        support: {
+            fileList: !!$('<input type="file">').prop('files'),
+            blobURLs: !!window.URL && URL.createObjectURL,
+            formData: !!window.FormData
+        },
+
+        init: function() {
+
+            this.support.datauri = this.support.fileList && this.support.blobURLs;
+
+            if (!this.support.formData) {
+                this.initIframe();
+            }
+
+            this.initModal();
+            this.addListener();
+        },
+
+        addListener: function() {
+            this.$avatarInput.on('change', $.proxy(this.change, this));
+            this.$avatarDone.on('click', $.proxy(this.cropDone, this));
+            // this.$avatarBtns.on('click', $.proxy(this.rotate, this));
+        },
+
+        initModal: function() {
+            this.$avatarModal.modal({
+                show: false
+            });
+        },
+
+        initIframe: function() {
+            var target = 'upload-iframe-' + (new Date()).getTime();
+            var $iframe = $('<iframe>').attr({
+                name: target,
+                src: ''
+            });
+            var _this = this;
+
+            // Ready ifrmae
+            $iframe.one('load', function() {
+
+                // respond response
+                $iframe.on('load', function() {
+                    var data;
+
+                    try {
+                        data = $(this).contents().find('body').text();
+                    } catch (e) {
+                        console.log(e.message);
+                    }
+
+                    if (data) {
+                        try {
+                            data = $.parseJSON(data);
+                        } catch (e) {
+                            console.log(e.message);
+                        }
+                        _this.submitDone(data);
+                    } else {
+                        _this.submitFail('Image upload failed!');
+                    }
+
+                    _this.submitEnd();
+
+                });
+            });
+
+            this.$iframe = $iframe;
+            this.$avatarForm.attr('target', target).after($iframe.hide());
+        },
+
+        change: function() {
+
+            var files;
+            var file;
+
+            //show modal
+            //this.$avatarModal.modal('show');
+
+            if (this.support.datauri) {
+
+                files = this.$avatarInput.prop('files');
+
+                if (files.length > 0) {
+                    file = files[0];
+
+                    //show modal
+                    this.$avatarModal.modal('show');
+
+                    //this is a image file
+                    if (this.isImageFile(file)) {
+                        if (this.url) {
+                            URL.revokeObjectURL(this.url); // Revoke the old one
+                        }
+
+                        this.url = URL.createObjectURL(file);
+                        this.startCropper();
+
+                    } else { // NOT a image file                        
+                        this.alert('Please upload an image file.');
+                    }
+                }
+            } else {
+                file = this.$avatarInput.val();
+                if (this.isImageFile(file)) {
+                    this.syncUpload();
+                }
+            }
+        },
+
+        rotate: function(e) {
+            var data;
+
+            if (this.active) {
+                data = $(e.target).data();
+
+                if (data.method) {
+                    this.$img.cropper(data.method, data.option);
+                }
+            }
+        },
+
+        isImageFile: function(file) {
+            if (file.type) {
+                return /^image\/\w+$/.test(file.type);
+            } else {
+                return /\.(jpg|jpeg|png|gif)$/.test(file);
+            }
+        },
+
+        startCropper: function() {
+            var _this = this;
+            if (this.active) {
+                this.$img.cropper('replace', this.url);
+            } else {
+                this.$img = $('<img src="' + this.url + '">');
+                this.$avatarWrapper.empty().html(this.$img);
+
+                //set all crop options
+                var cropOptions = this._jcropOptions;
+                cropOptions.preview = this.$avatarPreview.selector;
+                cropOptions.crop = function(e) {
+                    var json = [
+                        '{"x":' + e.x,
+                        '"y":' + e.y,
+                        '"height":' + e.height,
+                        '"width":' + e.width,
+                        '"rotate":' + (_this._jcropOptions.rotatable ? e.rotate : 0) + '}'
+                    ].join();
+
+                    _this.$avatarData.val(json);
+                };
+
+                //initialize cropper
+                this.$img.cropper(cropOptions);
+                this.active = true;
+            }
+
+            this.$avatarModal.one('hidden.bs.modal', function() {
+                _this.$avatarPreview.empty();
+                _this.clearAlert();
+                _this.stopCropper();
+            });
+        },
+
+        stopCropper: function() {
+            if (this.active) {
+                this.$img.cropper('destroy');
+                this.$img.remove();
+                this.active = false;
+            }
+        },
+
+        syncUpload: function() {
+            this.$avatarDone.click();
+        },
+
+        cropDone: function() {
+
+            if (!this.$avatarInput.val()) {
+                return false;
+            } else {
+
+                //fix height and width of the preview container
+                this.$avatarViewContainer.height(this.$avatarPreview.height());
+                this.$avatarViewContainer.width(this.$avatarPreview.width());
+
+                //add image to container
+                this.$avatarViewContainer.html(this.$avatarPreview.html());
+
+                //add a class to main container
+                this.$container.addClass('cropper-done');
+
+                this.stopCropper();
+                this.$avatarModal.modal('hide');
+            }
+        },
+
+        alert: function(msg) {
+            var $alert = [
+                '<div class="alert alert-danger cropper-alert alert-dismissable">',
+                '<button type="button" class="close" data-dismiss="alert">&times;</button>',
+                msg,
+                '</div>'
+            ].join('');
+
+            this.$avatarModalBody.prepend($alert);
+
+            //remove all error messages
+            var _this = this;
+            this.$avatarModal.one('hidden.bs.modal', function() {
+                _this.clearAlert();
+            });
+        },
+
+        clearAlert: function() {
+            this.$avatarModalBody.find('div.alert').remove();
+        }
+    };
+
+    //Extend jQuery with uploadCrop function.
+    $.fn.uploadCrop = function(options) {
+        new CropAvatar($(this), options);
+    }
+
+});
